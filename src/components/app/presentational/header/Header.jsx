3f38a1103e9d14465c395a12f40a63b18964dfc6
Katelinn Jones <katiejonesyo@gmail.com>
2021-02-11 14:59:31 -0800

    header start

---
+++ src/components/app/presentational/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Header {
+    render() {
+        return (
+            <div>
+               <h2>Welcome!</h2> 
+               <h3>To see more info on a character, just click on their image or name.</h3> 
+            </div>
+        )
+    }
+}
+
+
+

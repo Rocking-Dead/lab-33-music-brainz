6d20a40b045926222a1b087cd8f44778b14286fb
Rosalie Lee <sah-lee@Rosalies-MacBook-Pro.local>
2021-02-12 17:41:45 -0800

    added a hook to state folder

---
+++ src/state/useRecordings.js
@@ -0,0 +1,16 @@
+import { getRecordingById } from '../services/fetchMusicBrainzApi';
+import { useEffect, useState } from 'react'
+
+export const useRecording = (recordingId) => {
+    const [recordings, setRecording] = useState([]);
+
+    useEffect(() => {
+        getRecordingById(recordingId).then((recordings) => {
+            setRecording(recordings);
+        });
+    }, []);
+
+    return {
+        recordings,
+    };
+};

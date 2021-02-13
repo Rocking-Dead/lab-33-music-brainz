14a8f4ddf797afc8c90464b1f32ca03181dcf504
Rosalie Lee <sah-lee@Rosalies-MacBook-Pro.local>
2021-02-12 18:04:58 -0800

    added RecordingPage.jsx

--- src/components/presentational/recordingsPage/recordingsPage.jsx
+++ src/components/presentational/recordingsPage/recordingsPage.jsx
@@ -2,6 +2,27 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from 'react-router-dom';
 
-const recordingPage = ({ recordings, artist }) => {
-    const t
-}
+
+const RecordingPage = ({ recordings, artist }) => {
+    const recordingsList = recordings.map((recording) => {
+        return (
+        <li key={id}>
+            <Link to={`/releases/lyrics/${artist}/${recordingTitle}`}>
+                {recording.recordingTitle}
+            </Link>
+        </li>
+        );
+    });
+    return <ul data-testid="recordingsList">{recordingsList}</ul>
+}; 
+
+RecordingPage.propType = {
+    recordings: PropTypes.arrayOf(
+        PropType.shape({
+            recordingTitle: PropType.string.isRequired,
+        })
+        ),
+    artist: PropType.string.isRequired
+};
+
+export default RecordingPage;

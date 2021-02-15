417984022d7aaabc6dcc7004a253c77906bad683
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-14 16:55:22 -0800

    adds s to http in fetch calls

--- src/services/fetchMusicBrainzApi.js
+++ src/services/fetchMusicBrainzApi.js
@@ -1,23 +1,23 @@
 /* eslint-disable max-len */
 export const getAllArtists = (artist) => {
-  return fetch(`http://musicbrainz.org/ws/2/artist?query=${artist}&fmt=json&limit=25`)
+  return fetch(`https://musicbrainz.org/ws/2/artist?query=${artist}&fmt=json&limit=25`)
     .then(res => res.json())
     .then(json => json.artists);
 };
 
 export const getReleasesById = (id) => {
-  return fetch(`http://musicbrainz.org/ws/2/release?artist=${id}&fmt=json`)
+  return fetch(`https://musicbrainz.org/ws/2/release?artist=${id}&fmt=json`)
     .then((res) => res.json())
     .then((res) => res.releases)
     .then(releases => releases.map(release => ({
       id: release.id,
       title: release.title,
-      image: release['cover-art-archive']['front'] ? `http://coverartarchive.org/release/${release.id}/front` : 'https://www.placecage.com/100/100'
+      image: release['cover-art-archive']['front'] ? `https://coverartarchive.org/release/${release.id}/front` : 'https://www.placecage.com/100/100'
     })));
 };
 
 export const getRecordingById = (releaseId) => {
-  return fetch(`http://musicbrainz.org/ws/2/recording?release=${releaseId}&fmt=json`)
+  return fetch(`https://musicbrainz.org/ws/2/recording?release=${releaseId}&fmt=json`)
     .then(res => res.json())
     .then(json => json.recordings);
 };
